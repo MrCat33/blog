2b4e20b4f971d08bf236c5e34b334d67bab3b7ee
MrCat <1025090504@qq.com>
2023-01-10 14:36:57 +0800

    🐱 first add

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,35 @@
+export default {
+  title: 'MrCat',
+  description: 'webSite Blog',
+  themeConfig: {
+    nav: [
+      { text: "Guide", link: "/guide/" },
+    ],
+    sidebar: {
+      "/articles/": [
+        {
+          text: "组件库源码实现",
+          items: [
+            {
+              text: "组件库环境搭建",
+              link: "/articles/组件库环境搭建",
+            },
+            { text: "gulp的使用", link: "/articles/gulp的使用" },
+          ],
+        },
+        {
+          text: "vue教程",
+          items: [
+            {
+              text: "pina和vuex",
+              link: "/articles/pina和vuex",
+            },
+          ],
+        },
+      ],
+    },
+    socialLinks: [
+      { icon: "github", link: "https://github.com/MrCat33" }
+    ]
+  }
+}
